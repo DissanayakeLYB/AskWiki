2279b7cab4889aa4299f84e3d03433f2df841760
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-05-02 22:30:05 +0530

    Update 2_From_Wiki.py

--- pages/2_From_Wiki.py
+++ pages/2_From_Wiki.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 import streamlit as st
 from langchain.prompts import PromptTemplate
@@ -11,7 +11,7 @@ from langchain.chains import ConversationChain
 from langchain_groq import ChatGroq
 from groq import Groq
 
-groq_api_key = os.getenv('groq_API')
+groq_api_key = st.sidebar.text_input("Enter the GROQ API :")
 
 st.set_page_config(
     page_title = "AskWiki",
@@ -44,29 +44,30 @@ respond_template = PromptTemplate(
     template = "Write about the {topic} using {wikipedia_research} and mention the sources and links to them as well. If you do not have any information on {wikipedia_research} about {topic}, then reply with 'Sorry, I couldn't find any details in Wikipedia under {topic}."
 )
 
-# llms
-llm = ChatGroq(
-    api_key=groq_api_key ,
-    model_name=model,
-    temperature=0
-)
-respond_chain = LLMChain(llm=llm, prompt=respond_template, verbose=True)
+if groq_api_key:
+    # llms
+    llm = ChatGroq(
+        api_key=groq_api_key ,
+        model_name=model,
+        temperature=0
+    )
+    respond_chain = LLMChain(llm=llm, prompt=respond_template, verbose=True)
 
-wiki = WikipediaAPIWrapper()
+    wiki = WikipediaAPIWrapper()
 
-submit = st.button("Submit", key = "Enter")
+    submit = st.button("Submit", key = "Enter")
 
-# response
-if submit:
-    wiki_research = wiki.run(prompt)
-    response = respond_chain.run(topic=prompt , wikipedia_research = wiki_research)
+    # response
+    if submit:
+        wiki_research = wiki.run(prompt)
+        response = respond_chain.run(topic=prompt , wikipedia_research = wiki_research)
 
-    st.write(response)
+        st.write(response)
 
-    with st.expander('Wikipedia Research') :
-        st.info(wiki_research)   
+        with st.expander('Wikipedia Research') :
+            st.info(wiki_research)   
 
-        
+            
 
 
 
